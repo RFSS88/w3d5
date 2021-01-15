3dd4939a374dd4590bd182ddeb28f8c781f5526e
rafael silva <silvaserranor@gmail.com>
2021-01-15 14:24:56 -0500

    created #initilize and #parent

--- node_project/lib/00_tree_node.rb
+++ node_project/lib/00_tree_node.rb
@@ -1,3 +1,17 @@
 class PolyTreeNode
+    attr_reader :parent, :children, :value
+    
+    def initialize(value)
+        @value = value
+        @parent = nil 
+        @children = []
+    end
+
+     def parent=(new_parent)
+        self.parent = new_parent.value
+        new_parent.children << self.value
+
+     end
+
 
 end
