dad66bcd2b4ebf31c9bc239d5381e5c4d4777524
Solomon Bassalian <solomonbassalian@gmail.com>
2021-01-15 14:58:12 -0500

    idk

--- node_project/lib/00_tree_node.rb
+++ node_project/lib/00_tree_node.rb
@@ -8,10 +8,25 @@ class PolyTreeNode
     end
 
      def parent=(new_parent)
-        self.parent = new_parent.value
-        new_parent.children << self.value
-
+        @parent = new_parent
+        new_parent.children << self
      end
 
 
 end
+
+# n1 = PolyTreeNode.new("root1")
+# n2 = PolyTreeNode.new("root2")
+# n3 = PolyTreeNode.new("root3")
+
+# # connect n3 to n1
+# n3.parent = n1
+# # connect n3 to n2
+# n3.parent = n2
+
+# # this should work
+# raise "Bad parent=!" unless n3.parent == n2
+# raise "Bad parent=!" unless n2.children == [n3]
+
+# # this probably doesn't
+# raise "Bad parent=!" unless n1.children == []
